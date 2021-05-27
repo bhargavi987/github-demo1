b3d0a9fc90b82f86f38117f1b58077dbd627bb0e
bhargavi <maruboyenabhargavicse.gist@gmail.com>
2021-05-27 21:41:56 +0530

    evenodd.java and helloworld.java and multiplication.java added

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+class EvenOdd
+{
+public static void main(String args[])
+{
+int a=2;
+if(a%2==0)
+System.out.println("even number");
+else
+System.out.println("odd number");
+}
+}
---
+++ multiplication.java
@@ -0,0 +1,9 @@
+class Multiplication
+{
+public static void main(String args[])
+{
+int a=5,b=8;
+c=a*b;
+System.out.println("multiplication of two numbers:"+c);
+}
+}
