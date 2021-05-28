079cbb6b4401d7fec44927bdc5bdf9d428731f56
bhargavi <maruboyenabhargavicse.gist@gmail.com>
2021-05-28 12:29:08 +0530

    javaprograms added

--- multiplication.java
+++ multiplication.java
@@ -4,6 +4,6 @@ public static void main(String args[])
 {
 int a=5,b=8,c;
 c=a*b;
-System.out.println("multiplication of two numbers:"+c);
+System.out.println("Multiplication of two numbers:"+c);
 }
 }
