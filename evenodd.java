5341eaafb3ec2198b8fec4daef2216512eb075b0
bhargavi <maruboyenabhargavicse.gist@gmail.com>
2021-05-28 12:19:23 +0530

    java programs added

--- evenodd.java
+++ evenodd.java
@@ -2,8 +2,8 @@ public class EvenOdd
 {
 public static void main(String args[])
 {
-int a=2;
-if(a%2==0)
+int n=2;
+if(n%2==0)
 System.out.println("even number");
 else
 System.out.println("odd number");
