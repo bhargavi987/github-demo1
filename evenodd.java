1edd334616fda1179bf84c3dda522561ea567fcc
bhargavi <maruboyenabhargavicse.gist@gmail.com>
2021-05-28 11:01:33 +0530

    evenodd.java added

--- evenodd.java
+++ evenodd.java
@@ -1,4 +1,4 @@
-class EvenOdd
+public class EvenOdd
 {
 public static void main(String args[])
 {
--- multiplication.java
+++ multiplication.java
@@ -1,4 +1,4 @@
-class Multiplication
+public class Multiplication
 {
 public static void main(String args[])
 {
